4dd9d1e72e630aa01f1b616756c0bbdcf739b165
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-27 20:49:41 -0300

    criando a Page

---
+++ src/assets/page.ts
@@ -0,0 +1,28 @@
+export interface Page<T> {
+  content: T[];
+  pageable: {
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+    offset: number;
+    pageNumber: number;
+    pageSize: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  first: boolean;
+  sort: {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+  };
+  numberOfElements: number;
+  size: number;
+  number: number;
+  empty: boolean;
+}
