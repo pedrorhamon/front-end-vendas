35bee875b55e8791649f0c4695c4149c7520ba54
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-29 20:58:59 -0300

    ajustes no botao

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Credencias } from './model/credencias';
-import { catchError, map, Observable } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { Token } from './model/token';
 
 @Injectable({
@@ -11,9 +11,40 @@ export class LoginService {
 
   private baseUrl = 'http://localhost:8080/api/usuarios';
 
+  private isLoggedIn: boolean = false;
+  private userName: string = 'Pedro Rhamon';
+
+
   constructor(private http: HttpClient) { }
 
-  autenticar(credencias: Credencias): Observable<any> {
-    return this.http.post(`${this.baseUrl}/autenticar`, credencias);
+  autenticar(credenciais: Credencias): Observable<Token> {
+    return this.http.post<Token>(`${this.baseUrl}/autenticar`, credenciais)
+      .pipe(
+        tap(response => {
+
+          localStorage.setItem('token', response.token);
+        }),
+        catchError(error => {
+          // Trate o erro aqui, se necessário
+          return throwError(error);
+        })
+      );
   }
+
+  logout(): Observable<void> {
+    // Obter o token armazenado
+    const token = localStorage.getItem('token');
+    // Enviar o token no cabeçalho Authorization
+    return this.http.post<void>(`${this.baseUrl}/logout`, {}, {
+        headers: new HttpHeaders({
+            'Authorization': `Bearer ${token}`
+        })
+    }).pipe(
+        tap(() => {
+            // Remover o token após o logout
+            localStorage.removeItem('token');
+        })
+    );
+}
+
 }
--- src/app/template/navbar/navbar.component.ts
+++ src/app/template/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../login/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  userName: string = 'Pedro Rhamon'; // Inicialize com o nome do usuário
+  isAuthenticated: boolean = false; // Controle se o usuário está autenticado
 
+  constructor(private usuarioService: LoginService, private router: Router) {
+    // this.checkAuthentication();
+  }
+
+  logout() {
+    this.usuarioService.logout().subscribe(
+      () => {
+        console.log('Logout realizado com sucesso');
+        this.router.navigate(['']);
+      },
+      error => {
+        console.error('Erro ao realizar logout', error);
+      }
+    );
+  }
+
+  // checkAuthentication() {
+  //   this.isAuthenticated = !!this.usuarioService.getToken(); // Verifique se o token está presente
+  //   if (this.isAuthenticated) {
+  //     // Se necessário, recupere o nome do usuário do token ou de um serviço
+  //     this.userName = this.getUserNameFromToken(); // Método fictício para obter o nome
+  //   }
+  // }
 }
