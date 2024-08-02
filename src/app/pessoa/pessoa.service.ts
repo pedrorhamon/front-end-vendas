af7a2d3be0c91b4ce33b690271a6367aee896c55
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-08-01 21:06:19 -0300

    criando listarPessoa

---
+++ src/app/pessoa/pessoa.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pessoa } from './model/pessoa';
+import { Observable } from 'rxjs';
+import { Page } from '../../assets/page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaService {
+
+  private baseUrl = 'http://localhost:8080/api/pessoas';
+
+
+  constructor(private http: HttpClient) {}
+
+  listar(id?: number, name?: string, page: number = 0, size: number = 10): Observable<Page<Pessoa>> {
+    let params: any = {
+      page: page.toString(),
+      size: size.toString()
+    };
+
+    if (id !== undefined) {
+      params.id = id.toString();
+    }
+
+    if (name !== undefined) {
+      params.name = name;
+    }
+
+    return this.http.get<Page<Pessoa>>(this.baseUrl, { params });
+  }
+
+}
