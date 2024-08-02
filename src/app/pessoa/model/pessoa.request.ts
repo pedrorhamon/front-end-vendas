3be9ce56bb35f45fadd04577f0724a4d680efe42
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-08-01 21:03:04 -0300

    request também criado

---
+++ src/app/pessoa/model/pessoa.request.ts
@@ -0,0 +1,13 @@
+export interface PessoaRequest {
+  name: string;
+  ativo: boolean;
+  createdAt: Date;  // Datas como objetos Date
+  updatedAt: Date;  // Datas como objetos Date
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cep: string;
+  cidade: string;
+  estado: string;
+}
