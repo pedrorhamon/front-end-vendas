b7453daef8be29acccd3b78a538de5936682fa8c
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-28 18:30:09 -0300

    adicionando os bottons e acoes

--- src/app/permissao/permissao.component.ts
+++ src/app/permissao/permissao.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-permissao',
   templateUrl: './permissao.component.html',
   styleUrl: './permissao.component.css'
 })
-export class PermissaoComponent {
+export class PermissaoComponent implements OnInit{
+
+
+  constructor(
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: LoginService
+  ) {}
+
+   ngOnInit(): void {
+  }
+
+  canEdit(): boolean {
+    return this.authService.hasRole('ADMIN_PRIVILEGE');
+  }
+
+  canDelete(): boolean {
+    return this.authService.hasRole('ADMIN_PRIVILEGE');
+  }
+
+  canCreate(): boolean {
+    return this.authService.hasRole('ADMIN_PRIVILEGE');
+  }
 
 }
