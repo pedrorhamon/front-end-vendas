b91151411601bd57dc05753aa8ab3aaef0bf4872
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-28 20:04:38 -0300

    criando listarPessoa

--- src/app/permissao/permissao.component.ts
+++ src/app/permissao/permissao.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
+
 import { LoginService } from '../login/login.service';
 
 @Component({
--- src/app/permissao/permissao.service.ts
+++ src/app/permissao/permissao.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Permissao } from './model/permissao';
+import { Observable } from 'rxjs';
+import { Page } from '../../assets/page';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +12,33 @@ export class PermissaoService {
   private baseUrl = 'http://localhost:8080/api/permissoes';
 
   constructor(private http: HttpClient) { }
+
+
+  listarPessoa(id?: number, name?: string, page: number = 0, size: number = 10): Observable<Page<Permissao>> {
+    // let params: any = {
+    //   page: page.toString(),
+    //   size: size.toString()
+    // };
+    let params = new HttpParams()
+    .set('page', page.toString())
+    .set('size', size.toString());
+
+    if (id !== undefined) {
+      params = params.set('id', id.toString());
+    }
+
+    if (name !== undefined) {
+      params = params.set('name', name);
+    }
+
+    // if (id !== undefined) {
+    //   params.id = id.toString();
+    // }
+
+    // if (name !== undefined) {
+    //   params.name = name;
+    // }
+
+    return this.http.get<Page<Permissao>>(this.baseUrl, { params });
+  }
 }
