a1a1617680d7f605b416d807117877aa5b9399c9
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-30 11:20:32 -0300

    criando filter e a listagem

--- src/app/permissao/permissao.component.ts
+++ src/app/permissao/permissao.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
 
 import { LoginService } from '../login/login.service';
+import { Permissao } from './model/permissao';
+import { PermissaoService } from './permissao.service';
 
 @Component({
   selector: 'app-permissao',
@@ -11,16 +13,41 @@ import { LoginService } from '../login/login.service';
 })
 export class PermissaoComponent implements OnInit{
 
+  filteredPermissao: Permissao[] = [];
+  filterName: string = '';
+  permissoes: Permissao[] = [];
+  loading: boolean = true;
+  totalPages: number = 0;
+  page: number = 0;
+  size: number = 20;
+
 
   constructor(
     private confirmationService: ConfirmationService,
     private messageService: MessageService,
     private route: ActivatedRoute,
     private router: Router,
-    private authService: LoginService
+    private authService: LoginService,
+    private permissaoService: PermissaoService
   ) {}
 
    ngOnInit(): void {
+  }
+
+  listarPessoas(id?: number, name?: string): void {
+    this.permissaoService.listarPessoa(id, name, this.page, this.size).subscribe(page => {
+      this.permissoes = page.content;
+      this.totalPages = page.totalPages;
+      this.loading = false;
+
+      this.filterPessoas()
+    });
+  }
+
+  filterPessoas(): void {
+    this.filteredPermissao = this.permissoes.filter(permissao =>
+      permissao.name?.toLowerCase().includes(this.filterName.toLowerCase())
+    );
   }
 
   canEdit(): boolean {
