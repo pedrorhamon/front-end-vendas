56aa6e385fa3141cf4b2c3c3dbdff3f5a016c8dd
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-30 11:43:11 -0300

    listagem de subpermissao

--- src/app/permissao/sub-permissao/sub-permissao.service.ts
+++ src/app/permissao/sub-permissao/sub-permissao.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SubPermissao } from '../model/subPermissao';
+import { Page } from '../../../assets/page';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +12,33 @@ export class SubPermissaoService {
   private baseUrl = 'http://localhost:8080/api/sub-permissoes';
 
   constructor(private http: HttpClient) { }
+
+
+  listarPessoa(id?: number, name?: string, page: number = 0, size: number = 10): Observable<Page<SubPermissao>> {
+    // let params: any = {
+    //   page: page.toString(),
+    //   size: size.toString()
+    // };
+    let params = new HttpParams()
+    .set('page', page.toString())
+    .set('size', size.toString());
+
+    if (id !== undefined) {
+      params = params.set('id', id.toString());
+    }
+
+    if (name !== undefined) {
+      params = params.set('name', name);
+    }
+
+    // if (id !== undefined) {
+    //   params.id = id.toString();
+    // }
+
+    // if (name !== undefined) {
+    //   params.name = name;
+    // }
+
+    return this.http.get<Page<SubPermissao>>(this.baseUrl, { params });
+  }
 }
