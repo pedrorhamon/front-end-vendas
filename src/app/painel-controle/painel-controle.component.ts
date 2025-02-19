a41072fb7f278b2242222842f783fde0d6b122ce
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-02-18 23:42:58 -0300

    implementa a interface OnInit no componente painel de controle

--- src/app/painel-controle/painel-controle.component.ts
+++ src/app/painel-controle/painel-controle.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-painel-controle',
   templateUrl: './painel-controle.component.html',
   styleUrl: './painel-controle.component.css'
 })
-export class PainelControleComponent {
+export class PainelControleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
