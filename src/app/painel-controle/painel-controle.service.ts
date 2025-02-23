524de95fa4ca0d5e9b53ab78e7239bd20cd3e3aa
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-02-23 11:32:31 -0300

    adiciona métodos para obter e salvar configurações no serviço PainelControle

--- src/app/painel-controle/painel-controle.service.ts
+++ src/app/painel-controle/painel-controle.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PainelControleService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/configuracoes';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  obterConfiguracoes(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+
+  salvarConfiguracoes(configuracoes: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl, configuracoes);
+  }
 }
